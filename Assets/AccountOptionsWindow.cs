26be595e22a8f41d1712482ed87cb7ae799760c2
James Billy <james@epicguru.co.uk>
2017-11-19 20:38:43 +0100

    Lots of work on the editing system.

---
+++ Assets/AccountOptionsWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccountOptionsWindow : MonoBehaviour {
+
+    public static AccountOptionsWindow Instance;
+
+    public Text Name;
+
+    public Animator Animator;
+
+    public AccountOptionsWindow()
+    {
+        Instance = this;
+    }
+
+    public void UponOpen()
+    {
+        Animator.enabled = false;
+    }
+
+    public void UponClose()
+    {
+        Animator.enabled = false;
+        gameObject.SetActive(false);
+    }
+
+    public void TriggerOpen()
+    {
+        gameObject.SetActive(true);
+        Animator.SetBool("Open", true);
+        Animator.enabled = true;
+    }
+
+    public void TriggerClose()
+    {
+        gameObject.SetActive(true);
+        Animator.SetBool("Open", false);
+        Animator.enabled = true;
+    }
+}
---
+++ Assets/Scripts/User Interface/ChangeNameMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeNameMenu : MonoBehaviour {
+
+    public static ChangeNameMenu Instance;
+
+    public Animator Animator;
+
+    public ChangeNameMenu()
+    {
+        Instance = this;
+    }
+
+    public void TriggerOpen()
+    {
+        gameObject.SetActive(true);
+        Animator.SetBool("Open", true);
+        Animator.enabled = true;
+    }
+
+    public void TriggerClose()
+    {
+        Animator.SetBool("Open", false);
+        Animator.enabled = true;
+    }
+
+    public void AnimationOpen()
+    {
+        Animator.enabled = false;
+        (transform as RectTransform).rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    public void AnimationClose()
+    {
+        Animator.enabled = false;
+        gameObject.SetActive(false);
+    }
+}
