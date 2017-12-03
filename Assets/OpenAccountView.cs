9bfed4cc7a1d5ab0a4003f5126b7d04f9db9767c
James Billy <james@epicguru.co.uk>
2017-12-03 14:30:42 +0100

    Linked all the menus.

---
+++ Assets/OpenAccountView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenAccountView : MonoBehaviour {
+
+	public void Pressed()
+    {
+        Loading.Instance.SetOpen(true);
+
+        SceneManager.LoadSceneAsync("Account View", LoadSceneMode.Single);
+    }
+
+}
---
+++ Assets/ReturnToMenuButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenuButton : MonoBehaviour {
+
+	public void Pressed()
+    {
+        Loading.Instance.SetOpen(true);
+        SceneManager.LoadSceneAsync("Main Menu", LoadSceneMode.Single);
+    }
+
+}
