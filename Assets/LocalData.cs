445cd378290be3581b88267748414f95abe3ad43
James Billy <james@epicguru.co.uk>
2017-12-04 21:33:32 +0100

    Added basic file write functionality.

--- Assets/LocalData.cs
+++ Assets/LocalData.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LocalData : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static LocalData Instance;
+
+    public LocalData()
+    {
+        Instance = this;
+    }
+
+    public void Start()
+    {
+        Debug.Log(Application.persistentDataPath);
+    }
+
+    public bool WriteText(string path, string text)
+    {
+        try
+        {
+            StreamWriter writer = null;
+
+            writer = File.CreateText(path);
+
+            writer.Write(text);
+
+            writer.Close();
+
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Failed to write to file '" + path + "'! " + e);
+            return false;
+        }
+        
+    }
 }
